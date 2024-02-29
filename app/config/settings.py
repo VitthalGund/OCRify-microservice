b1fcd846971e55c08b4e6b3b56fd7dc0a381931f
VitthalGund <vitthalgund30@gmail.com>
2024-03-01 00:00:41 +0530

    config file added for setting configuration

---
+++ app/config/settings.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    app_auth_token: str
+    debug: bool = False
+    echo_active: bool = False
+    app_auth_token_prod: str = None
+    skip_auth: bool = False
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache
+def get_settings():
+    return Settings()
